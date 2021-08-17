92d05f68ca7d3403ba67629f44fe6188a58c4c70
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2021-08-16 22:10:12 -0300

    Cleaned css and html files from unused props

--- public/components/modal.jsx
+++ public/components/modal.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../stylesheets/modal.css'
 import '../stylesheets/form.css'
+import '../stylesheets/generalStructure.css'
 
 export default class Modal extends React.Component {
   constructor(props) {
@@ -12,8 +13,8 @@ export default class Modal extends React.Component {
       <div className="modal-container" id="modal-container">
         <div className="modal-body">
           <div className="modal-close" onClick={this.props.closeModal}>
-            <div className="modal-close--up-line"></div>
-            <div className="modal-close--down-line"></div>
+            <div className="close-button--up-line"></div>
+            <div className="close-button--down-line"></div>
           </div>
           <section className="modal-content">
             <header>
