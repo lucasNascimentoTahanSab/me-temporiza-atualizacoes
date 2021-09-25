6bd73802ba9527e975783d4cae066eb9e407daf3
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2021-09-25 16:02:06 -0300

    Converted webpack.config.js to ES6 conventions

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,27 @@
-const path = require('path')
+import { resolve as _resolve } from 'path'
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|css)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.(css)$/i,
-        use: ['style-loader', 'css-loader'],
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
-  entry: path.resolve(__dirname, './public/javascripts/index.js'),
-  output: {
-    publicPath: '/',
-    path: path.resolve(__dirname, './public/dist'),
-    filename: 'bundle.js',
-    library: 'bundleExport',
-    libraryTarget: 'window',
-    libraryExport: 'default'
-  }
+export const module = {
+  rules: [
+    {
+      test: /\.(js|jsx|css)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    },
+    {
+      test: /\.(css)$/i,
+      use: ['style-loader', 'css-loader'],
+    }
+  ]
+}
+export const resolve = {
+  extensions: ['*', '.js', '.jsx']
+}
+export const entry = _resolve(__dirname, './public/javascripts/index.js')
+export const output = {
+  publicPath: '/',
+  path: _resolve(__dirname, './public/dist'),
+  filename: 'bundle.js',
+  library: 'bundleExport',
+  libraryTarget: 'window',
+  libraryExport: 'default'
 }
