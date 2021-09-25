9ead8fbd293c441a37d78145f6b8545ef7dc7f45
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2021-09-25 16:28:05 -0300

    Set up express environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
-import { resolve as _resolve } from 'path'
+const path = require('path')
 
-export const module = {
-  rules: [
-    {
-      test: /\.(js|jsx|css)$/,
-      exclude: /node_modules/,
-      use: ['babel-loader']
-    },
-    {
-      test: /\.(css)$/i,
-      use: ['style-loader', 'css-loader'],
-    }
-  ]
-}
-export const resolve = {
-  extensions: ['*', '.js', '.jsx']
-}
-export const entry = _resolve(__dirname, './public/javascripts/index.js')
-export const output = {
-  publicPath: '/',
-  path: _resolve(__dirname, './public/dist'),
-  filename: 'bundle.js',
-  library: 'bundleExport',
-  libraryTarget: 'window',
-  libraryExport: 'default'
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|css)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css)$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  entry: path.resolve(__dirname, './public/javascripts/index.js'),
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, './public/dist'),
+    filename: 'bundle.js',
+    library: 'bundleExport',
+    libraryTarget: 'window',
+    libraryExport: 'default'
+  }
 }
