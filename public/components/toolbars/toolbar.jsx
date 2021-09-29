70371e72fe0c8a7ac2039eded0ef6a023e69ea66
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2021-09-28 22:21:26 -0300

    Created add button for the toolbar

--- public/components/toolbars/toolbar.jsx
+++ public/components/toolbars/toolbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import AddButton from '../buttons/addButton.jsx'
 import '../../stylesheets/form.css'
 import '../../stylesheets/toolbar.css'
 import '../../stylesheets/generalStructure.css'
@@ -16,7 +17,9 @@ export default class Toolbar extends React.Component {
           <div className="fill-halfly">
             <input className="form-input" id="task-search"></input>
           </div>
-          <div></div>
+          <div>
+            <AddButton></AddButton>
+          </div>
         </div>
       </div>
     )
