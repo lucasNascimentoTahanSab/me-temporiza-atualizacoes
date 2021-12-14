32def34ab0c7dc94d3035b10a8cf51078be0bc97
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2021-12-13 21:41:52 -0300

    Changes in modal for closing it when clicking out of it

--- public/components/modals/modal.jsx
+++ public/components/modals/modal.jsx
@@ -4,14 +4,27 @@ import '../../stylesheets/generalStructure.css'
 import '../../stylesheets/modal.css'
 
 export default class Modal extends React.Component {
+  _modal
+  _instanceOfCloseModalWhenClickingOut
+
   constructor(props) {
     super(props)
+    this._modal = React.createRef()
+    this._instanceOfCloseModalWhenClickingOut = this._closeModalWhenClickingOut.bind(this)
     this.state = { isVisible: true }
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this._instanceOfCloseModalWhenClickingOut)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this._instanceOfCloseModalWhenClickingOut)
   }
 
   render() {
     return (
-      <div className={"modal-body" + (this.state.isVisible ? "" : " hide")}>
+      <div className={"modal-body" + (this.state.isVisible ? "" : " hide")} ref={this._modal}>
         <div className="modal-close" onClick={this._closeModal.bind(this)}>
           <div className="close-button--up-line"></div>
           <div className="close-button--down-line"></div>
@@ -26,6 +39,12 @@ export default class Modal extends React.Component {
         </section>
       </div>
     )
+  }
+
+  _closeModalWhenClickingOut(event) {
+    if (this._modal.current.contains(event.target)) return
+
+    this._closeModal()
   }
 
   _closeModal() {
